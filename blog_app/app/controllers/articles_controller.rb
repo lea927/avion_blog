91f17915d2f8ad7ee5790dd68af24ae5f261ea43
lea927 <lxthym@icloud.com>
2021-07-15 23:26:44 +0800

    Adds create article feature

--- blog_app/app/controllers/articles_controller.rb
+++ blog_app/app/controllers/articles_controller.rb
@@ -7,9 +7,16 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    if @article.save!
+      redirect_to articles_path
+    else
+      render new_article_path
+    end
   end
 
   def edit
@@ -19,5 +26,9 @@ class ArticlesController < ApplicationController
   end
 
   def delete
+  end
+
+  def article_params
+    params.require(:article).permit(:name, :body)
   end
 end
