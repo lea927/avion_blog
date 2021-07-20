62ca2d1a1bbc42ffaef7fe1cfab94faac171e7f9
lea927 <lxthym@icloud.com>
2021-07-21 00:31:52 +0800

    Adds filter on Articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,10 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :delete]
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -21,24 +21,26 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     # raise params.inspect - prints out params on error page
     @article.update!(article_params)
     redirect_to article_path(@article)
   end
 
   def destroy
-    @article = Article.find(params[:id])
-
     @article.destroy
     redirect_to articles_path
   end
 
+  private 
+
   def article_params
     params.require(:article).permit(:name, :body)
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
   end
 end
