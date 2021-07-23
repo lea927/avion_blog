ca0657964d8db0e177d91cbce3a7d034d3a2ae43
lea927 <lxthym@icloud.com>
2021-07-24 00:29:13 +0800

    Adds favicon

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -7,10 +7,8 @@ import Rails from '@rails/ujs';
 import Turbolinks from 'turbolinks';
 import * as ActiveStorage from '@rails/activestorage';
 import 'channels';
-
 import 'bootstrap';
 import '../stylesheets/application';
-
 import '../js/bootstrap_js_files.js';
 
 Rails.start();
